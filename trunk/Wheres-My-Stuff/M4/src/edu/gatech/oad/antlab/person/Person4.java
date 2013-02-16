f7fa62f0a7cb8223289f8d4580b52dea5f6ac72c
emjackson37@gmail.com <emjackson37@gmail.com@e625c488-12fa-cc39-837b-05451d343e2b>
2013-02-16 01:20:01 +0000

    Reverting r25 | kylehosford@gmail.com | 2013-02-15 11:24:18 EST

---
+++ trunk/Wheres-My-Stuff/M4/src/edu/gatech/oad/antlab/person/Person4.java
@@ -0,0 +1,57 @@
+package edu.gatech.oad.antlab.person;
+
+/**
+ *  A simple class for person 4
+ *  returns their name and a
+ *  modified string 
+ *  
+ *  @author Bob
+ *  @version 1.1
+ */
+public class Person4 {
+  /** Holds the persons real name */
+  private String name;
+    /**
+     * The constructor, takes in the persons
+     * name
+     * @param pname the person's real name
+     */
+  public Person4(String pname) {
+    name = pname;
+  }
+    /**
+     * This method should take the string
+     * input and return its characters rotated
+     * 1 position.
+     * given "gtg123b" it should return
+     * "tg123bg".
+     *
+     * @param input the string to be modified
+     * @return the modified string
+     */
+    private String calc(String input) {
+      //Person 4 put your implementation here
+    	char[] letters = input.toCharArray();
+    	char firstLetter = letters[0];
+    	for (int i = 0; i< letters.length; i++ ){
+    		letters[i] = letters[i+1];
+    	}
+    	letters[length] = firstLetter;
+    	
+      return new String(letters);
+    }
+    
+    /**
+     * Return a string rep of this object
+     * that varies with an input string
+     *
+     * @param input the varying string
+     * @return the string representing the 
+     *         object
+     */
+    public String toString(String input) {
+      return name + calc(input);
+    }
+
+}
+
---
+++ trunk/Wheres-My-Stuff/M4/src/edu/gatech/oad/antlab/person/Person5.java
@@ -0,0 +1,53 @@
+package edu.gatech.oad.antlab.person;
+
+/**
+ *  A simple class for person 5
+ *  returns their name and a
+ *  modified string 
+ *  
+ *  @author Bob, Xinlu Wang
+ *  @version 1.1
+ */
+public class Person5 {
+  /** Holds the persons real name */
+  private String name;
+  	/**
+	 * The constructor, takes in the persons
+	 * name
+	 * @param pname the person's real name
+	 */
+  public Person5(String pname) {
+    name = pname;
+  }
+  	/**
+	 * This method should take the string
+	 * input and return its characters rotated
+	 * 3 positions.
+	 * given "gtg123b" it should return
+	 * "123bgtg".
+	 *
+	 * @param input the string to be modified
+	 * @return the modified string
+	 */
+	private String calc(String input) {
+	  //Person 5 put your implementation here
+		String output = "";
+		for(int i = 3; i < input.length(); i++)
+			output = output + input.charAt(i);
+		output = output + input.charAt(0) + input.charAt(1) + input.charAt(2);
+	  return output;
+	}
+	
+	/**
+	 * Return a string rep of this object
+	 * that varies with an input string
+	 *
+	 * @param input the varying string
+	 * @return the string representing the 
+	 *         object
+	 */
+	public String toString(String input) {
+	  return name + calc(input);
+	}
+
+}
