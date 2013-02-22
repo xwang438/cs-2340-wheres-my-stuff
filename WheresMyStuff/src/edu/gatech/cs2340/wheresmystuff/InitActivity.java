cc2f99202e82697708a68b33a02f686b70c1d5e7
xinlu0402@gmail.com <xinlu0402@gmail.com@e625c488-12fa-cc39-837b-05451d343e2b>
2013-02-22 18:00:19 +0000

    Initial Activity Start

--- WheresMyStuff/src/edu/gatech/cs2340/wheresmystuff/InitActivity.java
+++ WheresMyStuff/src/edu/gatech/cs2340/wheresmystuff/InitActivity.java
@@ -11,14 +11,14 @@ public class InitActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
+        getMenuInflater().inflate(R.menu.activity_login, menu);
         return true;
     }
     
