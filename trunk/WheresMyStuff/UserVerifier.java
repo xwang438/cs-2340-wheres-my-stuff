2da202c3ae1f2d26cfd763744e54151eed22a267
kylehosford@gmail.com <kylehosford@gmail.com@e625c488-12fa-cc39-837b-05451d343e2b>
2013-02-25 19:45:22 +0000

    Adding back-end handling for M5.

---
+++ trunk/WheresMyStuff/UserVerifier.java
@@ -0,0 +1,73 @@
+public class UserVerifier{
+  
+  private static String[] usernames = new String[1];
+  private static String[] passwords = new String[1];
+  private int userIndex;
+  
+  public UserVerifier(){
+    usernames[0] = "Admin1";
+    passwords[0] = "password1";
+    userIndex = -1;
+  }
+  
+  public String getUser(int index){
+    if(index > usernames.length){
+      System.out.println("Invalid Index");
+      return null;
+    }
+    else{
+      return usernames[index];
+    }
+  }
+  
+  public String getPassword(int index){
+    if(index > passwords.length){
+      System.out.println("Invalid Index");
+      return null;
+    }
+    else{
+      return passwords[index];
+    }
+  }
+  
+  public boolean checkUsername(String user){
+    for(int i = 0; i < usernames.length; i++){
+      if(usernames[i].equals(user)){
+        userIndex = i;
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  public boolean checkPassword(String pass){
+      if(passwords[userIndex].equals(pass)){
+        userIndex = -1;
+        return true;
+      }
+    return false;
+  }
+  
+  public void addUser(String newUser, String newPassword){
+    for(int i =0; i < usernames.length; i++){
+      if(usernames[i].equals(newUser)){
+        break;
+      }
+    }
+    String[] tempUsers = usernames;
+    usernames = new String[usernames.length+1];
+    String[] tempPass = passwords;
+    passwords = new String[passwords.length+1];
+    for(int i = 0; i < tempUsers.length; i++){
+      usernames[i] = tempUsers[i];
+      passwords[i] = tempPass[i];
+    }
+    usernames[usernames.length-1] = newUser;
+    passwords[passwords.length-1] = newPassword;
+    }
+      
+  
+  
+  
+  
+}
