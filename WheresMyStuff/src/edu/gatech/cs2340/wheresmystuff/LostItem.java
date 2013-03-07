a7d1255b5087fcb7019bcb3f8224d65678e02f1a
emjackson37@gmail.com <emjackson37@gmail.com@e625c488-12fa-cc39-837b-05451d343e2b>
2013-03-07 22:31:59 +0000

    Added java files from Bong

---
+++ WheresMyStuff/src/edu/gatech/cs2340/wheresmystuff/LostItem.java
@@ -0,0 +1,37 @@
+package edu.gatech.cs2340.wheresmystuff;
+
+public class LostItem {
+	String name = "";
+	int	id;
+	String description = "";
+
+	public LostItem() {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
