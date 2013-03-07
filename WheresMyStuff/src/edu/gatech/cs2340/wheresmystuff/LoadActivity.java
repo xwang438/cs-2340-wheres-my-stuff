9a4411afb735071a67c309851773f2920b5cf1ca
xinlu0402@gmail.com <xinlu0402@gmail.com@e625c488-12fa-cc39-837b-05451d343e2b>
2013-03-07 19:37:48 +0000

    Add LostActivity

--- WheresMyStuff/src/edu/gatech/cs2340/wheresmystuff/LoadActivity.java
+++ WheresMyStuff/src/edu/gatech/cs2340/wheresmystuff/LoadActivity.java
@@ -4,9 +4,7 @@ import android.os.Bundle;
 import android.content.Intent;  
 import android.graphics.PixelFormat;  
 import android.os.Handler;   
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+
 	  
 public class LoadActivity extends Activity {  
 	      
