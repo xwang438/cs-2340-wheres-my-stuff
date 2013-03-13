6de5d064db8e999d8d9456758d85fd0d067c43f8
emjackson37@gmail.com <emjackson37@gmail.com@e625c488-12fa-cc39-837b-05451d343e2b>
2013-03-13 16:28:53 +0000

    almost done, will add constructor chaining

--- trunk/WheresMyStuff/src/edu/gatech/cs2340/wheresmystuff/User.java
+++ trunk/WheresMyStuff/src/edu/gatech/cs2340/wheresmystuff/User.java
@@ -1,1 +1,47 @@
 package edu.gatech.cs2340.wheresmystuff;
+
+public class User{
+	//instance variables
+	String username;
+	String password;
+	String firstName;
+	String lastName;
+	Boolean locked = false;
+	Boolean admin = false;
+	
+	public User(String username, String password, String firstName, String lastName, Boolean locked, Boolean admin){
+		this.username = username;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.locked = locked;
+		this.admin = admin;
+	}
+	
+	//TODO: add constructor chaining
+	
+	public String getName(){
+		return firstName + " " + lastName;
+	}
+	
+	public void setFirstName(String firstName){
+		this.firstName = firstName;
+	}
+	
+	public void setLastName(String lastName){
+		this.lastName = lastName;
+	}
+	
+	public void setAsAdmin(){
+		admin = true;
+	}
+	
+	public void lockAccount(){
+		locked = true;
+	}
+	
+	public void unlockAccount(){
+		locked = false;
+	}
+	
+}
