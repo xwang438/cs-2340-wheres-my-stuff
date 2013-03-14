8193949cc524429f90995a3f3ab015feaac2cd6f
hari-seldon@live.com <hari-seldon@live.com@e625c488-12fa-cc39-837b-05451d343e2b>
2013-03-14 09:49:39 +0000

    Added AdminActivity java file.  Doesn't modify users yet, but still connects with everything else. -Kenneth

---
+++ trunk/WheresMyStuff/src/edu/gatech/cs2340/wheresmystuff/AdminActivity.java
@@ -0,0 +1,88 @@
+package edu.gatech.cs2340.wheresmystuff;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.content.Intent;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+/**
+ * The AdminActivity enables admins to modify user accounts. 
+ * @author Kenneth
+ *
+ */
+public class AdminActivity extends Activity {
+
+	private EditText userToModifyField;
+	private Button button,button1;
+	/**
+	 * On create, this method sets up all the instance variables of text from the UI. And sets up an on click listener
+	 * for the Submit button
+	 */
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_admin);
+		
+		userToModifyField = (EditText) findViewById(R.id.admin_textField);
+		
+
+		findViewById(R.id.admin_submit).setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						modifyUser();
+					}
+				});
+        /**
+         * Connect the AdminActivity with the ListActivity and the HomeActivity
+         */
+		this.button = (Button) this.findViewById(R.id.admin_submit);
+		this.button.setOnClickListener(new OnClickListener() 
+		{
+			@Override
+			public void onClick(View v) {
+		 
+				//LostItem newLost = new LostItem(userToModifyField.getText().toString());
+				//LostItem.item = newLost;
+				Intent intent = new Intent();
+				intent.setClass(AdminActivity.this,ListActivity.class);
+				startActivity(intent);
+			}
+		});
+		this.button1 = (Button) this.findViewById(R.id.admin_cancel);
+		this.button1.setOnClickListener(new OnClickListener() 
+		{
+			@Override
+			public void onClick(View v) {
+				Intent intent1 = new Intent();
+				intent1.setClass(AdminActivity.this,HomeActivity.class);
+				startActivity(intent1);
+			}
+		});
+	}
+
+	//@Override
+	//public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		//getMenuInflater().inflate(R.menu.activity_admin, menu);
+		//return true;
+	//}
+	
+	/**
+	 * 
+	 * 
+	 */
+	public void modifyUser(){
+		//Do something. 
+		
+		//String userToModify = userToModifyField.getText().toString();
+		
+		//Grab singleton object. uv.addUser(username, password);
+		
+	}
+
+}
