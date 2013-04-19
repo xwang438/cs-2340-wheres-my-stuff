f6003606b0a89fcd85c282b9819537f905cc3f0e
hari-seldon@live.com <hari-seldon@live.com@e625c488-12fa-cc39-837b-05451d343e2b>
2013-04-19 19:02:45 +0000

    Fixed VerifierTester. -Kenneth

--- WheresMyStuff/src/edu/gatech/cs2340/wheresmystuff/VerifierTester.java
+++ WheresMyStuff/src/edu/gatech/cs2340/wheresmystuff/VerifierTester.java
@@ -1,4 +1,7 @@
 package edu.gatech.cs2340.wheresmystuff;
+
+import android.test.ActivityInstrumentationTestCase2;
+
 /**
  * JUnit test class for UserVerifier
  * @author Kyle
@@ -7,24 +10,43 @@ package edu.gatech.cs2340.wheresmystuff;
  */
 
 
-public class VerifierTester {
+public class VerifierTester extends ActivityInstrumentationTestCase2<LoginActivity>{
 	
 	private static UserVerifier uv;
+	private Item item;
+	private Database db;
+	private LoginActivity la;
 	
+	/**
+	 * constructor for testing
+	 */
+	public VerifierTester() {
+		super(LoginActivity.class);
+	}
+
+	/**
+	 * get the LoginView Activity and add a new item named test
+	 */
+	protected void setUp() throws Exception {
+		la = getActivity();
+		item = new Item("test");
+		db.insertItem(item);
+		uv = new UserVerifier(la);
+	}
 	/**
 	 * Main method. When run it performs a simple test of the UserVerifier class.
 	 * specifically, it tests the UserVerifier class' ability to discern between existing
 	 * users, and users that have not been added to the database yet.
 	 */
 	public static void main(String[] args) {
-		uv = new UserVerifier();
+		
 		System.out.println("We will now check to see if UserVerifier can check a provided usename and password. usrname: Admin1, passwrd: password1.");
-		if(uv.loginCheck(Admin1, password1) == true){
+		if(uv.loginCheck("admin@gatech.edu", "admin1") == true){
 			System.out.println("the provided usename and input was correct. UserVerifier can successfully check preexisting users.");
 		}
 		else{System.out.println("UserVerifier failed to confirm a preexisting user. Have a nice day.");
 		System.out.println("We will now check to see if UserVerifier can catch a bad username and password. username: BaDnAmE, password: BaDwOrD.");
-		if(uv.loginCheck(BaDnAmE, BaDwOrD) == false){
+		if(uv.loginCheck("BaDnAmE@lol.com", "BaDwOrD") == false){
 			System.out.println("UserVerifier caught the bad username and password.");			
 		}
 		else{System.out.println("UserVerifier failed to catch the bad username and password.");
