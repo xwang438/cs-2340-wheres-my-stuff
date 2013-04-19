45e7805144a70e4289129f5b30fdef2944c0c56d
hari-seldon@live.com <hari-seldon@live.com@e625c488-12fa-cc39-837b-05451d343e2b>
2013-04-19 18:22:51 +0000

    Javadoc'd User. -Kenneth

--- WheresMyStuff/src/edu/gatech/cs2340/wheresmystuff/User.java
+++ WheresMyStuff/src/edu/gatech/cs2340/wheresmystuff/User.java
@@ -2,6 +2,17 @@ package edu.gatech.cs2340.wheresmystuff;
 
 import java.io.*;
 
+/**
+ * User.java
+ * Object to contain user information.
+ * @author Kenneth Craig
+ **/
+
+/**
+ * This class contains the user information of individual users
+ * for the "Where's My Stuff" App
+ **/
+
 public class User implements Serializable {
 	//instance variables
 	private int id;
@@ -44,58 +55,108 @@ public class User implements Serializable {
 		this(username, password, "New", "User", false, false);
 	}
 	
+	/**
+	 * @return id of this user.
+	 */
 	public int getID(){
 		return id;
 	}
+	
+	/**
+	 * @param i
+	 *            id to set this user to.
+	 */
 	
 	public void setID(int i){
 		this.id = i;
 	}
 	
+	/**
+	 * @return username of this user.
+	 */
 	public String getUsername(){
 		return username;
 	}
 	
+	/**
+	 * @param username
+	 *            username to set this user to.
+	 */
 	public void setUsername(String username){
 		this.username = username;
 	}
 	
+	/**
+	 * @return password of this user.
+	 */
 	public String getPassword(){
 		return password;
 	}
 	
+	/**
+	 * @param password
+	 *            password to set this user to.
+	 */
 	public void setPassword(String password){
 		this.password = password;
 	}
 	
+	/**
+	 * @return first name of this user.
+	 */
 	public String getFirstName(){
 		return firstName;
 	}
 	
+	/**
+	 * @param firstName
+	 *            first name to set this user to.
+	 */
 	public void setFirstName(String firstName){
 		this.firstName = firstName;
 	}
 	
+	/**
+	 * @return last name of this user.
+	 */
 	public String getLastName(){
 		return lastName;
 	}
 	
+	/**
+	 * @param lastName
+	 *            last name to set this user to.
+	 */
 	public void setLastName(String lastName){
 		this.lastName = lastName;
 	}
 	
+	/**
+	 * @return true if is admin, false if not admin.
+	 */
 	public boolean isAdmin(){
 		return admin;
 	}
 	
+	/**
+	 * @param a
+	 *            true to make admin, false to remove admin privileges
+	 */
 	public void setAdmin(boolean a){
 		admin = a;
 	}
 	
+	/**
+	 * @return true if locked, false if unlocked.
+	 */
 	public boolean isLocked(){
 		return locked;
 	}
 	
+	/**
+	 * @param l
+	 *            true to lock, false to unlock
+	 */
 	public void setLocked(boolean l){
 		locked = l;
 	}
